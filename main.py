a8415cb831f63220f7ea73f502b05e6c4598cb30
Wesley Kron <weskron@Wesleys-MacBook-Pro.local>
2026-07-25 10:24:11 -0700

    Initial Commit, building simulation structure.

---
+++ main.py
@@ -0,0 +1,2 @@
+# Main Sim runner
+
---
+++ reference_trajectory_generator.py
@@ -0,0 +1,18 @@
+class ReferenceTrajectoryGenerator:
+    def __init__(self):
+        # intitial state
+
+        # State transition matricies 
+
+        # State history
+
+        
+        pass
+    
+
+    def generate_trajectory(self):
+        # Placeholder implementation 
+        pass
+
+    def plot_trajectory(self):
+        pass
